169b92cbf107a81d1a3d8366943110a47a3728c4
Matt Rudin <matthias.rudin@live.com>
2019-06-01 15:39:51 +0200

    Refactoring: Add nested Util class

--- src/Warrior.java
+++ src/Warrior.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Warrior {
+    private final Util util = new Util();
     private int level;
     private int experience;
     private ranks rank;
@@ -9,8 +10,13 @@ public class Warrior {
 
     private enum ranks {
         Pushover, Novice, Fighter, Warrior, Veteran, Sage, Elite, Conqueror, Champion, Master, Greatest;
+
+        private static int currentRank = 0;
+        private static final int SIZE = 11;
+        public static ranks next() { return currentRank < SIZE ? values()[++currentRank] : ranks.Greatest; }
     }
 
+    // Constructor
     public Warrior() {
         level = 1;
         experience = 100;
@@ -18,6 +24,7 @@ public class Warrior {
         achievements = new ArrayList<>();
     }
 
+    // Getters and setters
     public int level() {
         return level;
     }
@@ -38,10 +45,15 @@ public class Warrior {
         experience += additionalExperience;
     }
 
+    private void increaseLevel() { level++; }
+
+    private void increaseRank() { rank = ranks.next(); }
+
     private void addAchievement(String description) {
         achievements.add(description);
     }
 
+    // Training method
     public String training(String description, int experience, int minimumLevel) {
         if (level >= minimumLevel) {
             setExperience(experience);
@@ -52,13 +64,23 @@ public class Warrior {
         }
     }
 
-    // The following methods will be used for battle
-    private boolean isLevelValid(int level) {
-        return level >= 1 && level <= 100;
+    // Battle method
+    public String battle(int enemyLevel) {
+        if (util.isLevelValid(enemyLevel)) {
+            return "A good fight";
+        } else {
+            return "Invalid level";
+        }
     }
 
-    private int calculateExperiencePoints(int experience, int level) {
-        int result = 0;
-        return result;
+    private static class Util {
+        private boolean isLevelValid(int level) {
+            return level >= 1 && level <= 100;
+        }
+
+        private int calculateExperiencePoints(int experience, int level) {
+            int result = 0;
+            return result;
+        }
     }
 }
