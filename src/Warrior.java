969800fe24266f46662c7a19ef43d1a459131896
Matt Rudin <matthias.rudin@live.com>
2019-06-02 13:03:45 +0200

    Add Util calculateRank method

--- src/Warrior.java
+++ src/Warrior.java
@@ -4,23 +4,15 @@ import java.util.List;
 public class Warrior {
     private int level;
     private int experience;
-    private ranks rank;
+    private Util.ranks rank;
     private final List<String> achievements;
     private final int MAX_EXPERIENCE = 10_000;
-
-    private enum ranks {
-        Pushover, Novice, Fighter, Warrior, Veteran, Sage, Elite, Conqueror, Champion, Master, Greatest;
-
-        private static int currentRank = 0;
-        private static final int SIZE = 11;
-        public static ranks next() { return currentRank < SIZE ? values()[++currentRank] : ranks.Greatest; }
-    }
 
     // Constructor
     public Warrior() {
         level = 1;
         experience = 100;
-        rank = ranks.Pushover;
+        rank = Util.ranks.Pushover;
         achievements = new ArrayList<>();
     }
 
@@ -28,26 +20,37 @@ public class Warrior {
     public int level() {
         return level;
     }
+
     public int experience() {
         return experience;
     }
+
     public String rank() {
         return rank.toString();
     }
+
     public List achievements() {
         return achievements;
     }
-    private void setLevel(int level) { this.level = level; }
-    private void increaseRank() { rank = ranks.next(); }
+
+    private void setLevel(int level) {
+        this.level = level;
+    }
+
     private void addAchievement(String description) {
         achievements.add(description);
     }
+
     private void setExperience(int additionalExperience) {
         if (experience + additionalExperience >= MAX_EXPERIENCE) {
             experience = MAX_EXPERIENCE;
         } else {
             experience += additionalExperience;
         }
+    }
+
+    private void setRank(Util.ranks rank) {
+        this.rank = rank;
     }
 
     // Training method
@@ -66,6 +69,7 @@ public class Warrior {
         if (Util.isLevelValid(enemyLevel)) {
             setExperience(Util.calculateExperiencePoints(level, enemyLevel));
             setLevel(Util.calculateLevel(experience, level));
+            setRank(Util.calculateRank(level));
             return Util.generateResponse(level, enemyLevel);
         } else {
             return "Invalid level";
@@ -77,10 +81,14 @@ public class Warrior {
         private static final int ONE_LEVEL_LOWER = 5;
         private static final int TWO_LEVEL_LOWER = 0;
         private static final int ACCELERATE_CONST = 20;
-        private static final int MAX_LEVEL= 100;
+        private static final int MAX_LEVEL = 100;
         private static final int MIN_LEVEL = 1;
         private static final int EXPERIENCE_THRESHOLD = 100;
+        private static final int LEVEL_THRESHOLD = 10;
 
+        private enum ranks {
+            Pushover, Novice, Fighter, Warrior, Veteran, Sage, Elite, Conqueror, Champion, Master, Greatest;
+        }
 
         private enum battleResponse {
             EASY("Easy fight"), GOOD("A good fight"), INTENSE("An intense fight");
@@ -125,6 +133,11 @@ public class Warrior {
 
         private static int calculateLevelDifference(int ownLevel, int enemyLevel) {
             return ownLevel - enemyLevel;
+        }
+
+        private static ranks calculateRank(int level) {
+            int normalizedLevel = level / LEVEL_THRESHOLD;
+            return ranks.values()[normalizedLevel];
         }
 
         private static String generateResponse(int ownLevel, int enemyLevel) {
