edbad3ce7e172250efd0b1920cb992d2b2dabf4b
Matt Rudin <matthias.rudin@live.com>
2019-06-04 19:43:40 +0200

    Add MysteryColorAnalyzer skeleton

---
+++ src/MysteryColorAnalyzerImpl.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class MysteryColorAnalyzerImpl implements MysteryColorAnalyzer {
+    @Override
+    public int numberOfDistinctColors(List<Color> mysteryColors) {
+        return 0;
+    }
+
+    @Override
+    public int colorOccurrence(List<Color> mysteryColors, Color color) {
+        return 0;
+    }
+}
